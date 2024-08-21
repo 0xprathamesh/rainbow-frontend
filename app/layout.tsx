5367e7c6a0e086b77020d94f1fad2c6a993000d8
0xprathamesh <prathameshpatil6499@gmail.com>
2024-08-21 14:13:24 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Config from "@/config";
 
 const inter = Inter({ subsets: ["latin"] });
 
